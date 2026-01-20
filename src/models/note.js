843e9ed512c6e0e30f5a7741292345c6d0c14edd
kopasovska <v.kopasovska@gmail.com>
2026-01-20 14:16:54 +0100

    Add CRUD operations, replace students with notes

---
+++ src/models/note.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { Student } from './models/student.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -15,37 +15,8 @@ app.use(logger);
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
-//PATHS
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-//-------------------------
-app.get('/students', async (req, res) => {
-  const students = await Student.find();
-  res.status(200).json(students);
-});
-
-app.get('/students/:studentID', async (req, res) => {
-  const { studentID } = req.params;
-  const student = await Student.findById(studentID);
-
-  if (!student) {
-    res.status(404).json({ message: 'Student not found!' });
-  }
-  res.status(200).json(student);
-});
-
-//-------------------------
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+//PATHS GROUP CONNECTION
+app.use(notesRoutes);
 
 //404 HANDLER
 app.use(notFoundHandler);
