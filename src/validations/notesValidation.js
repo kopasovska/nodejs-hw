d8ec438215af2e458c4683d88435c43a6e3c4236
kopasovska <v.kopasovska@gmail.com>
2026-01-27 12:07:22 +0100

    Add filtration

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -57,10 +57,10 @@ export const updateNoteSchema = {
 };
 
 export const getAllNotesSchema = {
-  [Segments.PARAMS]: Joi.object({
+  [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow('').trim(),
   }),
-
+};
