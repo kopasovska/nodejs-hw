07a78a402fe45933bd68d53f09c86ca3affb507d
kopasovska <v.kopasovska@gmail.com>
2026-02-15 14:03:30 +0100

    Add password reset and updating user's avatar functionality

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 
@@ -22,6 +23,7 @@ app.use(express.json({ limit: '10mb' }));
 //PATHS GROUP CONNECTION
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 //404 HANDLER
 app.use(notFoundHandler);
